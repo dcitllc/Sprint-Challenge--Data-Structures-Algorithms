0690e2c3efad7f13c5358e0cb606d774195077cc
Daniel Chamorro <dan@dcitllc.com>
2018-09-14 13:21:23 -0400

    Binary Search: Completed

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -1,46 +1,66 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def depth_first_for_each(self, cb):
-    pass    
+    def depth_first_for_each(self, cb):
+        stack = []
+        stack.append(self)
 
-  def breadth_first_for_each(self, cb):
-    pass
+        while len(stack):
+            current_node = stack.pop()
+            if current_node.right:
+                stack.append(current_node.right)
+            if current_node.left:
+                stack.append(current_node.left)
 
-  def insert(self, value):
-    new_tree = BinarySearchTree(value)
-    if (value < self.value):
-      if not self.left:
-        self.left = new_tree
-      else:
-        self.left.insert(value)
-    elif value >= self.value:
-      if not self.right:
-        self.right = new_tree
-      else:
-        self.right.insert(value)
+            cb(current_node.value)
 
-  def contains(self, target):
-    if self.value == target:
-      return True
-    if self.left:
-      if self.left.contains(target):
-        return True
-    if self.right:
-      if self.right.contains(target):
-        return True
-    return False
+    def breadth_first_for_each(self, cb):
+        queue = []
+        queue.append(self)
 
-  def get_max(self):
-    if not self:
-      return None
-    max_value = self.value
-    current = self
-    while current:
-      if current.value > max_value:
-        max_value = current.value
-      current = current.right
-    return max_value
+        while len(queue):
+            current_node = queue.pop(0)
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+
+            cb(current_node.value)
+
+    def insert(self, value):
+        new_tree = BinarySearchTree(value)
+        if (value < self.value):
+            if not self.left:
+                self.left = new_tree
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if not self.right:
+                self.right = new_tree
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if self.left:
+            if self.left.contains(target):
+                return True
+        if self.right:
+            if self.right.contains(target):
+                return True
+        return False
+
+    def get_max(self):
+        if not self:
+            return None
+        max_value = self.value
+        current = self
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.right
+        return max_value
